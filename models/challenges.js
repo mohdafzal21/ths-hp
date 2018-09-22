4e1241457a9116a1f53fdc8f28b5a94de73f4504
msfarookhy <msfarookhy@gmail.com>
2018-09-22 18:39:51 +0530

    UserSchema

---
+++ models/challenges.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+
+const challengesSchema = new mongoose.Schema({
+   
+        companyName: {
+            type: String,
+            required: true
+        },
+        position: {
+            type: String,
+            required: true
+        },
+        contestType: {
+            type: String,
+            required: true
+        },
+        startTime: {
+            type: String,
+            default: "00:00:00",
+        },
+        StartDate: {
+            type: Date,
+            default: Date.now(),
+        },
+
+        endTime: {
+            type: String,
+            default: "00:00:00",
+
+        },
+
+        endDate: {
+            type: Date,
+            default: Date.now(),
+
+        },
+        companyImage:{
+            type : String,
+
+        },
+        competitionImage : {
+            type : String,
+
+        },
+        jobAddress :{
+            type : String,
+        },
+        
+
+
+
+
+
+
+
+
+
+    
+
+})
+const Challenge = mongoose.model("Challenge", challengesSchema);
+
+module.exports = Challenge;
