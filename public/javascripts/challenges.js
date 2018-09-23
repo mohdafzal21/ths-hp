2e12a79230c326808c54e12dc4e39ed870da6375
Nabiji Noor <noornabiji4@gmail.com>
2018-09-24 00:46:55 +0530

    working at challenges branch

--- public/javascripts/challenges.js
+++ public/javascripts/challenges.js
@@ -75,13 +75,11 @@ challenges.generateMarkup = function() {
     // console.log(id);
 
     template += '<div class="card" style="width: 18rem;">';
-    template +=
-      '<img class="card-img-top" src="' + id.companyImage + '"></img>';
+    template +='<img class="card-img-top" src="' + id.companyImage + '"></img>';
     template += '<div class="card-body">';
     template += '<h3 class="card-title">' + id.companyName + "</h3>";
     template += '<p  class="card-text">' + id.position + "</p>";
-    template +=
-      '<h5 class="fas fa-map-marker-alt">' + id.jobAddress + "</h5><br>";
+    template += '<h5 class="fas fa-map-marker-alt">' + id.jobAddress + "</h5><br>";
     template += '<a href="#" class="btn btn-primary">Start</a>';
     template += '<button class="del btn btn-primary">delete</button>';
     template += '<button class="update btn btn-primary">update</button>';
